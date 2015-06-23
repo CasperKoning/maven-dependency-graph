15cb86119c8e9452cf8f34184c8a92c50681d010
CasperKoning <casper.e.koning@gmail.com>
2015-06-23 21:50:52 +0200

    extracted methods, included writing graph to disk

--- src/main/scala/nl/ordina/graph/MavenDependencyApp.scala
+++ src/main/scala/nl/ordina/graph/MavenDependencyApp.scala
@@ -2,31 +2,71 @@ package nl.ordina.graph
 
 import com.databricks.spark.csv._
 import org.apache.spark.graphx._
-import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MavenDependencyApp {
-  implicit val topRankOrdering = new Ordering[(MavenEntry, Double)] {
-    override def compare(a: (MavenEntry, Double), b: (MavenEntry, Double)) = (b._2 - a._2).toInt
-  }
 
   def main(args: Array[String]) {
+    checkForValidInput(args)
+    val sqlContext = initializeContext
+    val data = readDataFromFile(args(0), sqlContext)
+    val graph = constructGraphFromData(data)
+    val ranks = getPageRanks(graph)
+    displayPageRanks(ranks,graph.vertices)
+    saveGraphToDisk(graph,args(1))
+  }
+
+  def checkForValidInput(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      throw new IllegalStateException(
+        """
+          |Please supply the correct input:
+          |   args[0]: File path of input file.
+          |   args[1]: Output directory.
+        """.stripMargin)
+    }
+  }
+
+  private[this] def initializeContext: SQLContext = {
     val conf = new SparkConf().setAppName("Maven Dependency Graph")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
-    val data = sqlContext.tsvFile(args(0), useHeader = false)
+    sqlContext
+  }
+
+  private[this] def readDataFromFile(fileLocation: String, sqlContext: SQLContext): DataFrame = {
+    sqlContext.tsvFile(fileLocation, useHeader = false)
+  }
+
+  private[this] def constructGraphFromData(data: DataFrame): Graph[MavenEntry, String] = {
     val tuples = data.map(rowToTupleOfMavenEntryAndListOfDependencies)
     tuples.cache()
     val vertices = tuples.map(tuple => (tuple._1.getUniqueId, tuple._1))
     val edges = tuples.flatMap(establishRelationships)
-    val graph: Graph[MavenEntry, String] = Graph(vertices, edges)
-    val ranks = graph.pageRank(0.0001).vertices //FIXME magic number
-    val ranksByMavenEntry = vertices.join(ranks).map{
-        case (id, (mavenEntry, rank)) => (mavenEntry, rank)
-      }
-    ranksByMavenEntry.takeOrdered(25).zipWithIndex.foreach{
-      case ((entry,rank),index) => println(""+index+": " + entry + " with rank " + rank)
+    Graph(vertices, edges)
+  }
+
+  private[this] def getPageRanks(graph: Graph[MavenEntry, String]): VertexRDD[Double] = {
+    graph.pageRank(0.0001).vertices //FIXME magic number
+  }
+
+  private[this] def displayPageRanks(ranks: VertexRDD[Double],vertices: VertexRDD[MavenEntry]): Unit = {
+    implicit val topRankOrdering = new Ordering[(MavenEntry, Double)] {
+      override def compare(a: (MavenEntry, Double), b: (MavenEntry, Double)) = (b._2 - a._2).toInt
     }
+
+    val ranksByMavenEntry = vertices.join(ranks).map {
+      case (id, (mavenEntry, rank)) => (mavenEntry, rank)
+    }
+    ranksByMavenEntry.takeOrdered(25).zipWithIndex.foreach {
+      case ((entry, rank), index) => println("" + index + ": " + entry + " with rank " + rank)
+    }
+  }
+
+  private[this] def saveGraphToDisk(graph: Graph[MavenEntry, String], outputFolder: String) ={
+    graph.vertices.saveAsTextFile(outputFolder)
+    graph.edges.saveAsTextFile(outputFolder)
   }
 
   private[this] def establishRelationships(tuple: (MavenEntry, List[MavenEntry])): List[Edge[String]] = {
@@ -42,5 +82,4 @@ object MavenDependencyApp {
     val dependencies = MavenEntry.readBase64EncodedJSONtoListOfMavenEntries(row.getString(3))
     (entry, dependencies)
   }
-
 }
