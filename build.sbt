3fb74070f683205e8646c4d4a5b2dd69145e5e15
Casper Koning <casper.e.koning@gmail.com>
2016-11-06 21:53:53 +0100

    Working Spark 1.6 copy

--- build.sbt
+++ build.sbt
@@ -12,6 +12,8 @@ val graphx = "org.apache.spark" % "spark-graphx_2.10" % "1.4.0" % "provided"
 
 val sparkCsv = "com.databricks" % "spark-csv_2.10" % "1.1.0"
 
+val spray = "io.spray" %%  "spray-json"  % "1.3.2"
+
 val guava = "com.google.guava" % "guava" % "11.0"
 
 val scalaTest =  "org.scalatest" % "scalatest_2.10" % "2.2.5"
@@ -21,6 +23,7 @@ libraryDependencies ++= Seq(
   sparkSql,
   graphx,
   sparkCsv,
+  spray,
   guava,
   scalaTest
 )
--- src/main/scala/nl/ordina/graph/MavenEntry.scala
+++ src/main/scala/nl/ordina/graph/MavenEntry.scala
@@ -1,65 +1,52 @@
 package nl.ordina.graph
 
+import java.nio.charset.Charset
 import java.util.Base64
 
-import com.google.common.hash.{Hasher, Hashing}
-import org.apache.spark.sql.Row
+import com.google.common.hash._
+import org.apache.spark.sql._
+import spray.json._
 
-import scala.util.parsing.json.JSON
-
-case class MavenEntry(groupId: String, artifactId: String, version: String) {
-  /**
-   * I return a unique Id, defined by my state, in the shape of a Long.
-   */
+case class MavenEntry(groupId: Option[String] = Some("NO_GROUP_ID"), artifactId: Option[String] = Some("NO_ARTIFACT_ID"), version: Option[String] = Some("NO_VERSION")) {
   def getUniqueId: Long = {
     val hasher = Hashing.goodFastHash(64).newHasher()
 
-    if (groupId != null && !groupId.isEmpty) hasher.putString(groupId)
-    if (artifactId != null && !artifactId.isEmpty) hasher.putString(artifactId)
-    if (version != null && !version.isEmpty) hasher.putString(version)
+    hasher.putString(groupId.getOrElse("NO_GROUP_ID"), Charset.defaultCharset())
+    hasher.putString(artifactId.getOrElse("NO_ARTIFACT_ID"), Charset.defaultCharset())
+    hasher.putString(version.getOrElse("NO_VERSION"), Charset.defaultCharset())
 
     hasher.hash().asLong()
   }
 }
 
-object MavenEntry {
+object MavenEntry extends DefaultJsonProtocol {
+  implicit val mavenEntryFormat = jsonFormat(MavenEntry.apply, "groupid", "artifactid", "version")
   /**
-   * Utility method for reading a Base64 encoded String that contains a JSON-array of MavenEntries.
-   * For example, the Base64 String:
-   *
-   * W3sidmVyc2lvbiI6ICIwLjMuMCIsICJncm91cGlkIjogIm9yZy5zYWRpZnJhbWV3b3JrIiwgImFydGlmYWN0aWQiOiAic2FkaS1zZXJ2aWNlIn0sIH
-   * sidmVyc2lvbiI6ICIyLjAiLCAiZ3JvdXBpZCI6ICJvcmcuYXBhY2hlLm1hdmVuIiwgImFydGlmYWN0aWQiOiAibWF2ZW4tcGx1Z2luLWFwaSJ9LCB7I
-   * nZlcnNpb24iOiAiMi4wIiwgImdyb3VwaWQiOiAib3JnLmFwYWNoZS5tYXZlbiIsICJhcnRpZmFjdGlkIjogIm1hdmVuLXByb2plY3QifSwgeyJ2ZXJz
-   * aW9uIjogIjEuNi4yIiwgImdyb3VwaWQiOiAib3JnLmFwYWNoZS52ZWxvY2l0eSIsICJhcnRpZmFjdGlkIjogInZlbG9jaXR5In1d
-   *
-   * encodes the JSON
-   *
-   * [{"version": "0.3.0", "groupid": "org.sadiframework", "artifactid": "sadi-service"}, {"version": "2.0", "groupid":
-   * "org.apache.maven", "artifactid": "maven-plugin-api"}, {"version": "2.0", "groupid": "org.apache.maven",
-   * "artifactid": "maven-project"}, {"version": "1.6.2", "groupid": "org.apache.velocity", "artifactid": "velocity"}]
-   *
-   * which yields the list of MavenEntries
-   *
-   * List(MavenEntry(org.sadiframework,sadi-service,0.3.0), MavenEntry(org.apache.maven,maven-plugin-api,2.0),
-   * MavenEntry(org.apache.maven,maven-project,2.0), MavenEntry(org.apache.velocity,velocity,1.6.2))
-   *
-   * @param base64EncodedJSON the encoded string
-   * @return A list of MavenEntries that were encoded in the string
-   */
+    * Utility method for reading a Base64 encoded String that contains a JSON-array of MavenEntries.
+    * For example, the Base64 String:
+    *
+    * W3sidmVyc2lvbiI6ICIwLjMuMCIsICJncm91cGlkIjogIm9yZy5zYWRpZnJhbWV3b3JrIiwgImFydGlmYWN0aWQiOiAic2FkaS1zZXJ2aWNlIn0sIH
+    * sidmVyc2lvbiI6ICIyLjAiLCAiZ3JvdXBpZCI6ICJvcmcuYXBhY2hlLm1hdmVuIiwgImFydGlmYWN0aWQiOiAibWF2ZW4tcGx1Z2luLWFwaSJ9LCB7I
+    * nZlcnNpb24iOiAiMi4wIiwgImdyb3VwaWQiOiAib3JnLmFwYWNoZS5tYXZlbiIsICJhcnRpZmFjdGlkIjogIm1hdmVuLXByb2plY3QifSwgeyJ2ZXJz
+    * aW9uIjogIjEuNi4yIiwgImdyb3VwaWQiOiAib3JnLmFwYWNoZS52ZWxvY2l0eSIsICJhcnRpZmFjdGlkIjogInZlbG9jaXR5In1d
+    *
+    * encodes the JSON
+    *
+    * [{"version": "0.3.0", "groupid": "org.sadiframework", "artifactid": "sadi-service"}, {"version": "2.0", "groupid":
+    * "org.apache.maven", "artifactid": "maven-plugin-api"}, {"version": "2.0", "groupid": "org.apache.maven",
+    * "artifactid": "maven-project"}, {"version": "1.6.2", "groupid": "org.apache.velocity", "artifactid": "velocity"}]
+    *
+    * which yields the list of MavenEntries
+    *
+    * List(MavenEntry(org.sadiframework,sadi-service,0.3.0), MavenEntry(org.apache.maven,maven-plugin-api,2.0),
+    * MavenEntry(org.apache.maven,maven-project,2.0), MavenEntry(org.apache.velocity,velocity,1.6.2))
+    *
+    * @param base64EncodedJSON the encoded string
+    * @return A list of MavenEntries that were encoded in the string
+    */
   def readBase64EncodedJSONtoListOfMavenEntries(base64EncodedJSON: String): List[MavenEntry] = {
     val json = new String(Base64.getDecoder.decode(base64EncodedJSON))
-    JSON.parseFull(json) match {
-      case Some(x: List[Map[String, String]]) => x map getMavenEntry
-      case _ => List()
-    }
-  }
-
-  private[this] def getMavenEntry(mavenMap: Map[String, String]): MavenEntry = {
-    MavenEntry(
-      mavenMap.getOrElse("groupid", "NO_GROUP_ID"),
-      mavenMap.getOrElse("artifactid", "NO_ARTIFACT_ID"),
-      mavenMap.getOrElse("version", "NO_VERSION")
-    )
+    json.parseJson.convertTo[List[MavenEntry]]
   }
 
   def getDependenciesFromRow(row: Row): List[MavenEntry] = {
@@ -67,6 +54,6 @@ object MavenEntry {
   }
 
   def getFirstMavenEntryFromRow(row: Row): MavenEntry = {
-    MavenEntry(row.getString(0), row.getString(1), row.getString(2))
+    MavenEntry(Option(row.getString(0)), Option(row.getString(1)), Option(row.getString(2)))
   }
 }
